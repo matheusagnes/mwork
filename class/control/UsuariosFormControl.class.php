f5e2ea56793d31db021af4939eb6e4f0292d2b4b
matheusagnes <matheusagnes@debfd200-988d-c415-8c6a-44e1301c4d76>
2012-04-18 19:36:57 +0000

    Quase salvando, e jah validando

--- class/control/UsuariosFormControl.class.php
+++ class/control/UsuariosFormControl.class.php
@@ -5,10 +5,19 @@ class UsuariosFormControl
 
 	public static function save()
 	{
-		#FIXME como pegar a view ? de uma forma facil para checar automaticamente os campos
-		$control = new MControl(2);
-		$objPost = $control->getPost();
-		var_dump($control, $objPost->nome);
+		
+		$control = new MControl();
+		
+		if($control->save())
+		{
+			echo 'salvo';
+		}
+		else
+		{
+			echo $control->getError();
+		}
+
+	
 	}
 
 
