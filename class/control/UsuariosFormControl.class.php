0761254c0afee1c5daa1b82818818f0ae5e99cb6
arenhart.daniel@gmail.com <arenhart.daniel@gmail.com@debfd200-988d-c415-8c6a-44e1301c4d76>
2012-04-19 16:20:31 +0000

    Alteracoes

--- class/control/UsuariosFormControl.class.php
+++ class/control/UsuariosFormControl.class.php
@@ -1,27 +1,9 @@
 <?php
 
-class UsuariosFormControl
+class UsuariosFormControl extends MControl
 {
 
-	public static function save()
-	{
-        for($i=0;$i<10000000;$i++){$a++;}
-		
-		$control = new MControl();
-		
-		// Caso queira utilizar a save padrão
-		if($control->save())
-		{
-			echo 'salvo';
-		}
-		else
-		{
-			echo $control->getError();
-		}
-
-	
-	}
-
+    
 
 }
 
