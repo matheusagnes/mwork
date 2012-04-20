fc57269257f3b5b52b9e3b7a353ad494db43c27e
matheusagnes <matheusagnes@debfd200-988d-c415-8c6a-44e1301c4d76>
2012-04-20 16:40:30 +0000

    Ajustes na MForm, setControlName

--- trunk/framework/MCore.php
+++ trunk/framework/MCore.php
@@ -26,7 +26,7 @@ class MCore
         require_once 'include.php';
         require_once 'configs.php';
         require_once 'lib/php/tools.php';
-        require_once 'lib/DB.php';
+        require_once 'DB.php';
 
 		$this->projectName = $mgProjectName;
 		$this->siteName = $mgSiteName;
