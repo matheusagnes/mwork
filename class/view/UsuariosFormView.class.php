82f5859ca795d92a62607ce3f9cb4e29210e3c4d
matheusagnes <matheusagnes@debfd200-988d-c415-8c6a-44e1301c4d76>
2012-04-30 03:09:15 +0000

    ajustes na grid

--- class/view/UsuariosFormView.class.php
+++ class/view/UsuariosFormView.class.php
@@ -7,9 +7,19 @@ class UsuariosFormView extends MForm
     {
         parent::__construct('usuarios', 'Cadastro de Usuários', false);
 
+        $ref_tipo_usuarios = new MCombo();
+        
+        $objs = DB::getObjects('select * from tipos_usuarios');
+        foreach ($objs as $obj)
+        {
+            $ref_tipo_usuarios->addItem($obj->id, $obj->descricao);
+        }
+        
+        
         parent::addField('nome', 'Nome', new MText(), true);
         parent::addField('email', 'E-mail', new MText(), true);
         parent::addField('senha', 'Senha  ', new MText(), true);
+        parent::addField('ref_tipo_usuario', 'Tipo Usuário  ', $ref_tipo_usuarios, true);
     }
 
 }
