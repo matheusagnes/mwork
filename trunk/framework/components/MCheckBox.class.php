3597c7219571c7b25aa6d566e2682be8e73bcbce
matheusagnes <matheusagnes@debfd200-988d-c415-8c6a-44e1301c4d76>
2012-09-14 23:55:31 +0000

    ajustes, css, checkbox

--- trunk/framework/components/MCheckBox.class.php
+++ trunk/framework/components/MCheckBox.class.php
@@ -23,7 +23,7 @@ class MCheckBox extends MInput
     {
         $this->checked = $checked;
     }
-
+    // array com objChecks contendo value, label, property e checked
     function addChecks($aChecks)
     {
         $this->aChecks = $aChecks;
@@ -42,19 +42,37 @@ class MCheckBox extends MInput
                 $props .= ' ' . $key . '=\'' . $value . '\'';
             }
         }
-
        
-
         unset($checked);
         if ($this->checked)
         {
             $checked = " checked = '1' ";
         }
-        if(!$this->value)
-        {
-            $this->value = 1;
+        
+        if(!is_array($this->value))
+        {         
+            $this->value = 1;            
         }
-        $htmlCheckBox = "<input id='{$this->name}' name='{$this->name}' value='{$this->value}' type='checkbox'  $checked $props >\n";
+
+        if($this->aChecks)
+        {
+            $cont = 0;
+            foreach ($this->aChecks as $check) 
+            {    
+                $checked = '';                       
+                if($check->checked || $this->value[$check->value])
+                {
+                    $checked = " checked = '1' ";
+                }                                 
+                
+                $cont++;
+                $htmlCheckBox .= "<div> <input class='check_input' id='{$this->name}_{$cont}' {$check->property} name='{$this->name}[]' value='{$check->value}' type='checkbox'  $checked $props > <label style='width:auto; padding:2px;font-weight: normal;' class = 'check_label' for='{$this->name}_{$cont}'> {$check->label} </label> </div>";
+            }
+        }
+        else
+        {
+            $htmlCheckBox = "<input id='{$this->name}' name='{$this->name}' value='{$this->value}' type='checkbox'  $checked $props >";
+        }
         //$htmlCheckBox .= "<label for='{$this->name}'> {$this->labelCheck} </label>";
         return $htmlCheckBox;
     }
